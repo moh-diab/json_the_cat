aa423dcf4f8118acfbcd0cb7d1cce928647d3aed
moh-diab <mohdiab@gmail.com>
2021-12-02 23:08:25 -0500

    Refactoring breedFetcher and index files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
 const request = require('request');
-const breedName = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (body === '[]') {
-    console.log(`This breed can not be found.`);
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (body === '[]') {
+      return callback (`This breed can not be found.`, null);
 
-  } else if (error) {
-    console.log('error: ', error);
+    } else if (error) {
+      return callback (error, null);
 
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0]["description"]);
-  };
-});
+    } else {
+      const data = JSON.parse(body);
+      return callback (null, data[0]["description"]);
+    };
+  });
+};
+
+module.exports = { fetchBreedDescription };
